2ec3fc3b063a338b0b3b50ce3455599d50bda87d
Nadine Seibel <nadine.seibel@bi-so.de>
2023-03-21 13:35:28 +0100

    blindly tried fixing deprecated stuff and updated mlkit

--- src/ios/CameraViewController.m
+++ src/ios/CameraViewController.m
@@ -353,7 +353,10 @@ didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer
     double focus_y = (touchPoint.y+66)/self.previewLayer.frame.size.height;
 
     NSError *error;
-    NSArray *devices = [AVCaptureDevice devices];
+    NSArray *devices = [AVCaptureDeviceDiscoverySession
+                        discoverySessionWithDeviceTypes:captureDeviceType
+                        mediaType:AVMediaTypeVideo
+                        position:AVCaptureDevicePositionUnspecified];
     for (AVCaptureDevice *device in devices){
         NSLog(@"Device name: %@", [device localizedName]);
         if ([device hasMediaType:AVMediaTypeVideo]) {
@@ -483,7 +486,10 @@ static inline double radians (double degrees) {return degrees * M_PI/180;}
 }
 
 - (AVCaptureDeviceInput *)captureDeviceInputForPosition:(AVCaptureDevicePosition)desiredPosition {
-    for (AVCaptureDevice *device in [AVCaptureDevice devicesWithMediaType:AVMediaTypeVideo]) {
+    for (AVCaptureDevice *device in [AVCaptureDeviceDiscoverySession
+                                     discoverySessionWithDeviceTypes:captureDeviceType
+                                     mediaType:AVMediaTypeVideo
+                                     position:AVCaptureDevicePositionUnspecified]) {
         if (device.position == desiredPosition) {
             NSError *error = nil;
             AVCaptureDeviceInput *input = [AVCaptureDeviceInput deviceInputWithDevice:device
