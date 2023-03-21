3fa1ede519283eafb3c419022ab39fcf7ae96205
Nadine Seibel <nadine.seibel@bi-so.de>
2023-03-21 11:52:44 +0100

    removed all the company stuff that was hardcoded...

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,5 @@
-/** @type {import('@teckel.ts-internal/eslint-config/Types').IEslintConfig} */
 const config = {
   env: { node: true },
-  extends: '@teckel.ts-internal/eslint-config',
   parserOptions: {
     project: './tsconfig.lint.json',
     tsconfigRootDir: __dirname,
